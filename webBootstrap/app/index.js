c66fe6816ac8bd74458559e0de31e7ea6c4b680c
Xiaohang Su <sxhdragon@gmail.com>
2017-04-09 23:00:16 -0400

    Movies Row Component

--- webBootstrap/app/index.js
+++ webBootstrap/app/index.js
@@ -1,2 +1,31 @@
 require('./../style/scss/style.scss');
 import Vue from './vue';
+import moviesRow from './moviesRow';
+
+new moviesRow({
+    el: '#recommend-movies-row', // under index.html element id == #recommend-movies-row
+    data: {
+        title: 'Recommend Movies',
+        movies: [
+            {text:'test'},
+            {text:'test'},
+            {text:'test'},
+            {text:'test'},
+            {text:'test'}
+        ]
+    }
+});
+
+new moviesRow({
+    el: '#new-movies-row', // under index.html element id == #recommend-movies-row
+    data: {
+        title: 'Newest Movies',
+        movies: [
+            {text:'test'},
+            {text:'test'},
+            {text:'test'},
+            {text:'test'},
+            {text:'test'}
+        ]
+    }
+});
